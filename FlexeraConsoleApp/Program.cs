84e4f71f609475f06be449c043f4b8aec636b2f3
Nico Valero <Nico.v@Totalcoding.co.uk>
2021-08-15 12:39:32 +0100

    Agregar archivos de proyecto.

---
+++ FlexeraConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlexeraConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
